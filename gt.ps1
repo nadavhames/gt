d2e177d27dbfe37c290d4df937f404e3aa43dc41
Nadav Hames <25757773+nadavhames@users.noreply.github.com>
2024-02-14 18:32:58 +0200

    Update gt.ps1

--- gt.ps1
+++ gt.ps1
@@ -1,39 +1,50 @@
-# Hashtable to store mappings of words to locations
-$script:LocationMappings = @{
-    "dev" = "C:\dev"
-    # Add more shortcuts as needed
+# Define the path to the JSON file that will store the bookmarks
+$bookmarkFile = "$HOME\bookmarks.json"
+
+# Ensure the bookmark file exists
+if (!(Test-Path -Path $bookmarkFile)) {
+    @{} | ConvertTo-Json | Set-Content -Path $bookmarkFile
 }
 
-# Function to quickly change to predefined directories with autocomplete
-function GoTo {
+function gt {
     param (
-        [string]$Location,
-        [switch]$List  # -list prints script:locationmappings
+        [string]$command,
+        [string]$name
     )
 
-    if ($List) {
-        # Print the mappings to the console
-        $script:LocationMappings | Format-Table -AutoSize
-    } elseif (-not $Location)  {
-        #select a location using fzf
-        $selectedLocation = $script:LocationMappings.Keys | fzf --ansi
-        if ($selectedLocation) {
-            Set-Location $script:LocationMappings[$selectedLocation]
+    # Load the bookmarks from the JSON file
+    $bookmarks = Get-Content -Path $bookmarkFile | ConvertFrom-Json
+
+    switch ($command) {
+        "mark" {
+            # Add the current directory as a bookmark
+            if (!$bookmarks.$name) {
+                $bookmarks | Add-Member -NotePropertyName $name -NotePropertyValue (Get-Location).Path
+            } else {
+                $bookmarks.$name = (Get-Location).Path
+            }
+            $bookmarks | ConvertTo-Json | Set-Content -Path $bookmarkFile
         }
-    } elseif ($script:LocationMappings.ContainsKey($Location)) {
-        Set-Location $script:LocationMappings[$Location]
-    } else {
-        Write-Host "Unknown location. Available options: $($script:LocationMappings.Keys -join ", ") "
-    }
-}
-
-# Define the argument completer for the GoTo function using the mappings
-Register-ArgumentCompleter -CommandName GoTo -ParameterName Location -ScriptBlock {
-    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
-
-    if ($wordToComplete) {
-        $script:LocationMappings.Keys -like "$wordToComplete*"
-    } else {
-        $script:LocationMappings.Keys
+        "remove" {
+            # Remove the bookmark
+            if ($bookmarks.$name) {
+                $bookmarks.PSObject.Properties.Remove($name)
+                $bookmarks | ConvertTo-Json | Set-Content -Path $bookmarkFile
+            } else {
+                Write-Host "Bookmark '$name' does not exist."
+            }
+        }
+        default {
+            # If no command is specified, change to the directory of the bookmark
+            if ($bookmarks.$command) {
+                Set-Location -Path $bookmarks.$command
+            } else {
+                # If the bookmark doesn't exist, open fzf to select a bookmark
+                $selectedBookmark = $bookmarks.PSObject.Properties.Name | fzf
+                if ($selectedBookmark) {
+                    Set-Location -Path $bookmarks.$selectedBookmark
+                }
+            }
+        }
     }
 }
